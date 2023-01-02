181e6af58be717d6ccdb70d812a1ccfafd0df948
Petra <petragrunheidt@gmail.com>
2023-01-02 19:15:41 -0300

    exibição basica de dados a partir de busca por token

--- public/main.js
+++ public/main.js
@@ -33,3 +33,42 @@ function buildTable(page) {
   button.remove();
 }
 
+
+const tokenInput = document.getElementById('token-input');
+const tokenButton = document.getElementById('token-button');
+
+
+tokenButton.addEventListener('click', function() {
+  const params = tokenInput.value;
+  fetch(`http://localhost:3000/api/test/${params}`)
+    .then(response => response.json())
+    .then(data => {
+      const patientData = Object.fromEntries(Object.entries(data).slice(0,5))
+      const medicData = data['médico']
+      const examsData = data['exames']
+
+      div = document.getElementById('results-token')
+      const h2 = document.createElement('h2');
+      h2.textContent = `Resultados da busca por: ${data['token']}`
+      div.appendChild(h2);
+      const dl = document.createElement('dl')
+     
+      for (var property in patientData) {
+        const dt = document.createElement('dt')
+        dt.textContent = `${property}: ${patientData[property]}`
+        dl.appendChild(dt)
+      };
+      for (var property in medicData) {
+        const dt = document.createElement('dt')
+        dt.textContent = `${property}: ${medicData[property]}`
+        dl.appendChild(dt)
+      }
+      for (var i = 0; i < examsData.length; i++) {
+        for (var property in examsData[i]) {
+          const dt = document.createElement('dt')
+          dt.textContent = `${property}: ${examsData[i][property]}`
+          dl.appendChild(dt)
+        }}
+      div.appendChild(dl);
+    });
+});
